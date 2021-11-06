eaefbb6ae010357b756351a9b7b735d8363911e5
Tanesal <dguchinsky@yahoo.com>
2021-11-06 13:46:39 +0300

    refactor: async/await axios requests

--- bff-service/src/app.ts
+++ bff-service/src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // app.use(loggerMiddleware);
 
-app.all('/*', (req, res, next) => {
+app.all('/*', async (req, res, next) => {
     console.log('originalUrl', req.originalUrl);
     console.log('method', req.method);
     console.log('body', req.body);
@@ -22,35 +22,57 @@ app.all('/*', (req, res, next) => {
 
     const {originalUrl, body} = req;
     const recipient = originalUrl && originalUrl.split('/')[1];
-    // const recipientUrl = recipient && process.env[recipient];
     
+    // const recipientUrl = recipient && process.env[recipient];
     const recipientUrl = recipient && recipients.get(recipient);
 
     console.log('recipient', `${recipientUrl}${req.originalUrl}`);
 
     if(recipientUrl){
-        const axiosConfig = {
-            method: req.method as Method,
-            url: `${recipientUrl}${req.originalUrl}`,
-            ...((Object.keys(body) || {}).length > 0 && {data: body})
-        };
 
-        axios(axiosConfig)
-        .then( function(response){
+        try {
+            const axiosConfig = {
+                method: req.method as Method,
+                url: `${recipientUrl}${req.originalUrl}`,
+                ...((Object.keys(body) || {}).length > 0 && {data: body})
+            };
+
+            const response = await axios(axiosConfig);
+
             console.log('Response from recipient', response.data);
             res.send(response.data);
-            }
-        ).catch(error => {
+
+        } catch (error) {
+
             console.log('Recipient request failed', JSON.stringify(error));
 
-            if(error.response){
-                const {status, data} = error.response;
+            // if(error.response){
+            //     const {status, data} = error.response;
 
-                res.status(status).json(data);
-            }else{
-                res.status(500).json({error: error.message});
-            }
-        });
+            //     res.status(status).json(data);
+            // }else{
+            //     res.status(500).json({error: error.message});
+            // }
+            
+        }
+
+        // axios(axiosConfig)
+        // .then( function(response){
+        //     console.log('Response from recipient', response.data);
+        //     res.send(response.data);
+        //     }
+        // ).catch(error => {
+            
+        //     console.log('Recipient request failed', JSON.stringify(error));
+
+        //     if(error.response){
+        //         const {status, data} = error.response;
+
+        //         res.status(status).json(data);
+        //     }else{
+        //         res.status(500).json({error: error.message});
+        //     }
+        // });
     }else{
         res.status(502).json({error: 'Cannot process request'});
     }
